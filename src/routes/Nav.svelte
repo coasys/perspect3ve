1fc77e1a5f3f698a7639465dee9a21c915d54218
Nicolas Luck <nicolas@lucksus.eu>
2023-03-04 09:37:20 +0100

    Nav bar showing perspective circle with name and event dispatching

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { getAd4mClient } from '@perspect3vism/ad4m-connect';
-    import { onMount } from 'svelte';
+  import { getAd4mClient } from '@perspect3vism/ad4m-connect';
+  import { onMount, createEventDispatcher } from 'svelte';
+
     let refs
 
     let ad4m;
@@ -14,14 +15,11 @@
     
     let selected = navItems[0].id;
     
+    const dispatch = createEventDispatcher();
+
     function handleSelect(id) {
       selected = id;
       dispatch('select', id);
-    }
-  
-    function dispatch(eventName, detail) {
-      const event = new CustomEvent(eventName, { detail });
-      dispatchEvent(event);
     }
     
     let navHeight = 0;
@@ -33,69 +31,90 @@
       perspectives = await ad4m.perspective.all()
       console.log(perspectives)
     });
-  </script>
+</script>
   
-  <div class="nav-container" >
-    <ul bind:this={refs} class="nav">
-      {#each perspectives as p}
-        <li
-          class="nav-item {p.uuid === selected ? 'selected' : ''}"
-          on:click={() => handleSelect(p.uuid)}
-        >
-          <div class="nav-circle"></div>
-          <div class="nav-label">{p.name}</div>
-        </li>
-      {/each}
-    </ul>
-  </div>
+<div class="nav-container" >
+  <ul bind:this={refs} class="nav">
+    {#each perspectives as p}
+      {@const displayText = p.name.length > 0 ? p.name : p.uuid}
+      <button
+        class="nav-item {selected === p.uuid ? 'selected' : ''}"
+        on:click={() => handleSelect(p.uuid)}
+      >
+        <span class="nav-item-text">
+
+          {#if displayText.length > 10}
+            {displayText.slice(0, 5)}...
+          {:else}
+            {displayText}
+          {/if}
+        </span>
+      </button>
+    {/each}
+  </ul>
+</div>
   
-  <style>
-    .nav-container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      overflow: scroll;
-      width: 80px;
-      padding: 20px;
-      background-color: #2c2f33;
-    }
-    
-    .nav {
-      list-style: none;
-      margin: 0;
-      padding: 0;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
-    
-    .nav-item {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-      margin-bottom: 10px;
-      cursor: pointer;
-    }
-    
-    .nav-item.selected {
-      background-color: #7289da;
-      border-radius: 50px;
-    }
-    
-    .nav-circle {
-      width: 10px;
-      height: 10px;
-      border-radius: 50%;
-      background-color: #b9bbbe;
-      margin-right: 10px;
-    }
-    
-    .nav-label {
-      font-size: 14px;
-      color: #b9bbbe;
-      text-align: center;
-    }
-  </style>
+<style>
+  .nav-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: scroll;
+    width: 80px;
+    padding: 20px;
+    background-color: #2c2f33;
+  }
+  
+  .nav {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .nav-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    margin: 10px;
+    border-radius: 50%;
+    cursor: pointer;
+    background-color: #ffffff;
+    color: #282c34;
+    font-weight: bold;
+    text-align: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+.nav-item.selected {
+  background-color: #61dafb;
+  color: #282c34;
+}
+
+.nav-item-text {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+  
+  .nav-circle {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: #b9bbbe;
+    margin-right: 10px;
+  }
+  
+  .nav-label {
+    font-size: 14px;
+    color: #b9bbbe;
+    text-align: center;
+  }
+</style>
   
