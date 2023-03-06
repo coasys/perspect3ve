104068d8d0e6c0e0f25480e118aa71825c5db87f
Nicolas Luck <nicolas@lucksus.eu>
2023-03-06 14:50:47 +0100

    Clean-up unused code

--- src/routes/MainView.svelte
+++ src/routes/MainView.svelte
@@ -18,140 +18,12 @@
 
   let perspective: PerspectiveProxy | undefined;
 
-  
-
   let history: HistoryElement[] = [];
-  let children = new Map<string, Set<string>>();
   let coords = new Map<string, { x: number; y: number }>();
-  let containers = new Map<string, PIXI.Container>();
   let currentExpression: string | null = null;
 
   let selectedExpression = null
   const dispatch = createEventDispatcher();
-
-  async function findCoordsLink(expr: string, parent: string): Promise<LinkExpression | null> {
-    const results = await perspective.get({ source: parent, target: expr })
-    return results.find((link) => link.data.predicate.startsWith("p3://child_coords_2d"))
-  }
-
-  async function getCoords(expr: string, parent: string): Promise<{ x: number; y: number }> {
-    const key = `${parent} -> ${expr}`;
-
-    if (!coords.has(key)) {
-      const link = await findCoordsLink(expr, parent)
-      if(link) {
-        const payload = link.data.predicate.substring(COORDS_PRED_PREFIX.length)
-        const point = JSON.parse(payload)
-        coords.set(key, { x: point.x, y: point.y })
-        console.log('found coords for:', key, point)
-        return point
-      }
-    }
-    console.warn('no coords found for:', key)
-    return { x: 0, y: 0 }
-    
-  }
-
-  async function updateCoords(expr: string, parent: string, point: { x: number; y: number }) {
-    const key = `${parent} -> ${expr}`;
-    coords.set(key, point);
-    let link = await findCoordsLink(expr, parent)
-    while(link) {
-      await perspective.remove(link)
-      link = await findCoordsLink(expr, parent)
-    }
-    const payload = JSON.stringify({x: point.x, y: point.y})
-    const predicate = `${COORDS_PRED_PREFIX}${payload}`
-    await perspective.add({ source: parent, target: expr, predicate })
-  }
-
-  async function updateChildren(expr: string) {
-    //debugger
-    if (perspective) {
-      const result = await perspective.get({ source: expr });
-      if (result) {
-        const childrenArray = result.map((link) => link.data.target);
-        const childrenSet = new Set(childrenArray);
-        children.set(expr, childrenSet);
-       //console.log('updated children for:', expr);
-      }
-    } else {
-      console.warn('updateChildren called before perspective was set');
-    }
-  }
-
-  function _updateChildren(expr: string, currentTreeItem: any) {
-    if (currentTreeItem.id === expr) {
-      if (currentTreeItem.children) {
-        children.set(
-          expr,
-          currentTreeItem.children.map((child) => child.id)
-        );
-      }
-      return true;
-    }
-    if (currentTreeItem.children) {
-      for (let child of currentTreeItem.children) {
-        if (_updateChildren(expr, child)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  function renderExpressionLayer(
-    expression: string,
-    layer: PIXI.Container,
-    renderChildren: boolean = false
-  ) {
-    containers.set(expression, layer);
-    layer.addChild(createExpressionCircle(expression == selectedExpression), createTextNode(expression));
-    if (renderChildren) 
-      renderChildrenLayers(expression, layer)
-    else
-      renderChildrenCircles(expression, layer)
-  }
-
-  function updateExpressionLayer(expression: string) {
-    let layer = containers.get(expression)
-    if(layer) {
-      layer.children.forEach(child => {
-        layer?.removeChild(child)
-      })
-      renderExpressionLayer(expression, layer, false)
-    }
-  }
-
-  function renderChildrenCircles(expression: string, layer: PIXI.Container) {
-    console.log('renderChildrenCircles', expression);
-    //updateChildren(expression);
-    console.log('children:', children.get(expression));
-    children.get(expression)?.forEach(async (child) => {
-      let point = await getCoords(child, expression)
-      if (!point) {
-        console.error('no point for child:', child);
-        return;
-      }
-
-      console.log('adding child:', child, 'at', point);
-      let childLayer = new PIXI.Container();
-      childLayer.position.set(point.x, point.y);
-      childLayer.scale.set(LEVEL_SCALE);
-      childLayer.addChild(createExpressionCircle(), createTextNode(child));
-      layer.addChild(childLayer);
-    });
-  }
-
-
-
-
-  // Define the zooming animation duration
-  const zoomDuration = 50;
-
-  // Define the pan speed and boundaries
-  const panSpeed = 100;
-  const panBounds = new PIXI.Rectangle(-1000, -1000, 2000, 2000);
 
   function setupLayers(expr: string) {
     coords.clear();
@@ -200,11 +72,6 @@
     if(perspectiveID) {
       const ad4m = await getAd4mClient();
       perspective = await ad4m.perspective.byUUID(perspectiveID);
-      children.clear();
-      coords.clear();
-      await updateChildren('ad4m://self');
-      //await updateCoords('ad4m://self');
-      //debugger
       setupLayers('ad4m://self');
     } else {
       setupLayers('')
